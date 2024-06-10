9ad1844db778069ddb94eb487e9e73a354c97f90
alfredoperry14 <alfredoperry14@gmail.com>
2024-06-10 11:38:28 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alfredo Perry on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
